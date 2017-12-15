58c370efc17317e7b2bc60b7967cf6613ea7127a
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-12-15 23:07:24 +0300

    Added new config for gulp. Less, sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,29 @@
+'use strict';
+
 const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const less = require('gulp-less');
 const browserSync = require('browser-sync');
 
+
+/* Dev 
+================== */
+
+/* Style */
+gulp.task('dev:styles', function() {
+
+  return gulp.src('./less/styles.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'))
+});
+
+
+/* Build 
+=======================*/
+
+/* Sync */
 gulp.task('sync', function() {
   browserSync.init({
     server: {
@@ -9,5 +32,17 @@ gulp.task('sync', function() {
   });
 
   browserSync.watch('./css/*.css').on('change', browserSync.reload);
-  browserSync.watch('./index.html').on('change', browserSync.reload);
+  browserSync.watch('./js/*.js').on('change', browserSync.reload);
+  browserSync.watch('./*.html').on('change', browserSync.reload);
 });
+
+/* Watch */
+gulp.task('watch', function() {
+  gulp.watch('./less/**/*.less', gulp.series('dev:styles')); // watch styles
+});
+
+
+/* Dev */
+gulp.task('dev', gulp.series(
+  gulp.parallel('watch', 'sync')
+));
