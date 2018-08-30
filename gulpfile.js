2b74e13aee125f2b87531db6a8ae88acf5927f9c
Alex Dolzhenkov <alexgred.dev@gmail.com>
2018-08-30 20:46:50 +0300

    Restruct project

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const less = require('gulp-less');
 const browserSync = require('browser-sync');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
 
 /* Dev 
@@ -12,12 +15,15 @@ const browserSync = require('browser-sync');
 /* Style */
 gulp.task('dev:styles', function() {
 
-  return gulp.src('./less/styles.less')
+  return gulp.src('./src/less/styles.less')
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'))
 });
+
+
+
 
 
 /* Build 
@@ -35,14 +41,14 @@ gulp.task('sync', function() {
     }
   });
 
-  browserSync.watch('./css/*.css').on('change', browserSync.reload);
-  browserSync.watch('./js/*.js').on('change', browserSync.reload);
+  browserSync.watch('./src/css/*.css').on('change', browserSync.reload);
+  browserSync.watch('./src/js/*.js').on('change', browserSync.reload);
   browserSync.watch('./*.html').on('change', browserSync.reload);
 });
 
 /* Watch */
 gulp.task('watch', function() {
-  gulp.watch('./less/**/*.less', gulp.series('dev:styles')); // watch styles
+  gulp.watch('./src/less/**/*.less', gulp.series('dev:styles')); // watch styles
 });
 
 
