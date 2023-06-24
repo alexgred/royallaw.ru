a8db738a848276d340e8b2dfffe52eaef3fc4a3d
Alex Dolzhenkov <alexgred.dev@gmail.com>
2023-06-25 00:01:05 +0300

    add nav

---
+++ src/components/Navigation.vue
@@ -0,0 +1,125 @@
+<template>
+  <nav class="navbar navbar-default navbar-fixed-top sc-hidden" role="navigation">
+    <div class="container">
+      <g-link class="navbar-brand" to="/"></g-link>
+      <div class="logo-block">
+        <g-link to="/">
+          <g-image alt="Logo" src="~/favicon.png" width="90" />
+        </g-link>
+      </div>
+      <div class="site-name-block">
+        <div class="site-name">
+          <h1>{{ props.siteName }}</h1>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse navbar-main-menu">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <g-link class="nav__link" to="/">Главная</g-link>
+          </li>
+          <li>
+            <g-link class="nav__link" to="/price">Прайс-лист</g-link>
+          </li>
+          <li>
+            <a class="anchor" href="#block-contacts">Контакты</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+const props = defineProps(['siteName'])
+</script>
+
+<style lang="scss">
+@import '~/assets/scss/_variables.scss';
+
+.navbar {
+  border-bottom: 1px solid $navbar-border;
+  height: $navbar-height;
+  background-color: $navbar-bg;
+  
+  &.affix {
+    box-shadow: 0 0 7px rgba(0, 0, 0, 0.25);
+
+    .logo-block {
+
+      img {
+        height: 60px;
+      }
+    }
+  }
+
+  .navbar-main-menu {
+    float: right;
+  }
+
+  .nav {
+    height: $navbar-height;
+    padding: 0;
+
+    a {
+      font-weight: 100;
+      padding: 0 15px;
+      line-height: $navbar-height;
+      height: $navbar-height;
+      border-bottom: 3px solid transparent;
+      transition: all 0.3s;
+
+      &.active {
+        border-bottom: 3px solid $color-second;
+      }
+    }
+  }
+}
+
+.site-name-block {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+
+  .site-name {
+    font-family: 'Crimson Text';
+    font-size: 40px;
+    line-height: 80px;
+    font-weight: 600;
+    color: $color-site-name;
+    text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+    white-space: nowrap;
+
+    h1 {
+      font-size: 100%;
+      margin: 0;
+      line-height: 80px;
+      font-weight: 600;
+    }
+  }
+}
+
+.logo-block {
+  float: left;
+  padding: 10px 0;
+  margin-left: -35px;
+
+  @media #{$s} {
+    margin-left: 20px;
+  }
+
+  a {
+    height: $navbar-height - 20px;
+    display: inline-block;
+  }
+
+  img {
+    transition: all 0.15s;
+    height: 60px;
+
+    @media #{$s} {
+      height: 90px;
+    }
+  }
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,35 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
+  <div>
+
+    <Navigation :siteName="$static.metadata.siteName" />
+
+    <header class="header sc-hidden">
+      <div class="container">
+        
+      </div>
     </header>
-    <slot/>
+
+    <div class="content">
+
+    </div>
+
+    <div class="scroll-to-top"></div>
+
+    <footer class="footer sc-hidden">
+
+    </footer>
   </div>
 </template>
+
+<script>
+import Navigation from '@/components/Navigation';
+
+export default {
+  components: {
+    Navigation,
+  },
+}
+</script>
 
 <static-query>
 query {
@@ -22,29 +40,4 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
 </style>
