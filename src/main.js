c5a071bfb622b5ab4163f548930269ce1fe9ed51
Alex Dolzhenkov <alexgred.dev@gmail.com>
2023-06-25 01:17:32 +0300

    fixes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue';
+
 import '~/assets/bootstrap/css/bootstrap.min.css';
+// import '~/assets/fonts/font-awesome/css/font-awesome.css';
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
